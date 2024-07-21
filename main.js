cc722f43b7c0a3767feae4b49f69f1eb991e83fd
milk19948 <97678058+Milk25@users.noreply.github.com>
2024-07-21 15:22:06 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+let items = ['phones: 99.99', 'tablet: 999.99', 'pen: 29.99']
+
+let userLoggedIn = true
+
+let items2 = items.push("Laptop: 3999.99")
+
+if (userLoggedIn === true){
+    console.log("Im in!")
+}else{
+    console.log("Out of luck")
+}
+
+
+for(let i = 0; i < length.items2; i++){
+    if (items != items){
+        console.log("Bad luck")
+    }else if (items === items2){
+        console.log("Great!")
+    } else {
+        console.log("Outdated")
+    }
+}
+
+totalCost = 0
+function displayCart(){
+    totalCost = items2
+    console.log(totalCost)
+}
+
+bankDeposits = 0
+function deposits(){
+    let bankDeposits = totalCost
+    if (bankDeposits == totalCost){
+        console.log("A whole lot of money")
+    } else { (bankDeposits != totalCost){
+        console.log("Insufficient Funds")
+    }
+}
+
+function withdraws(){
+    let (bankDeposits != totalCost)
+        console.log("No getting item")
+    }
+}
+
+function currentBalance(){
+    if (bankDeposits == totalCost)
+        console.log("Yes, I've gotten my stuff")
+}
+
+displayCart()
+deposits()
+withdraws()
+currentBalance()
